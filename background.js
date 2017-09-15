5f89b013902bb1311afa94f436b1342c233e1d10
G-Webber <g.webber.bsch@gmail.com>
2017-09-15 08:32:54 +0200

    add background
    this contains the handler for the app activation cick

---
+++ background.js
@@ -0,0 +1,12 @@
+
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript(tab.ib, {
+		file: 'the_b_d.js'
+	});
+});
